6a212e03a6f8530a4f84b81c91731dcc0aa3ef1d
aya bassi <pkarbassi@hotmail.co.uk>
2023-08-25 15:46:01 +0100

    Added an image person.fill instead of hello world tex

--- GitPractice/ContentView.swift
+++ GitPractice/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!!")
+            Image(systemName: "person.fill")
         }
     }
 }
