fae6a2cfa334e71acf25dcc1ac20236ae6984661
aya bassi <pkarbassi@hotmail.co.uk>
2023-08-25 15:33:30 +0100

    First commit

---
+++ GitPractice/GitPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitPracticeApp.swift
+//  GitPractice
+//
+//  Created by Aya Bassi on 25/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct GitPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
